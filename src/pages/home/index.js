efacefa914917445199ccbe7d763eaeba4c0d151
quan.nd <vuvivan11@gmail.com>
2022-07-19 11:29:37 +0700

    fix routes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { Route } from 'react-router-dom';
-import SidebarOverlay from '~/components/SidebarOverlay';
+import SidebarOverlay from '~/components/sidebarOverlay';
 import CartOverLay from '~/components/CartOverlay';
 
 export default function Home({ exact, path, component }) {
